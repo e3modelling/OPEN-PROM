a630f34b965bb38ba8840d721314a04490ffeb79
derevirn <derevirn@gmail.com>
2023-10-16 23:19:17 +0300

    Added data validation with iamc

--- ReportData.R
+++ ReportData.R
@@ -1,8 +1,12 @@
 library(gdx)
 library(quitte)
+library(iamc)
 
-var <- readGDX('blabla.gdx', 'VDemTr', field = 'l')
+var <- readGDX('./blabla.gdx', 'VDemTr', field = 'l')
 var_q <- as.quitte(var)
 
-write.mif(var_q, 'VDemTr.mif')
+# write.mif(var_q, 'VDemTr.mif')
 
+iamCheck(var_q, pdf = "./report.pdf",
+         refData = "./Data_validation_mrprom.mif",
+         verbose = FALSE)
