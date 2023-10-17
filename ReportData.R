8f1deccf9a512e80f58f092dd118261a739e3525
derevirn <derevirn@gmail.com>
2023-10-17 22:31:09 +0300

    Created quitte files for GDP and Population

--- ReportData.R
+++ ReportData.R
@@ -1,12 +1,25 @@
+library(dplyr)
 library(gdx)
 library(quitte)
 library(iamc)
 
-var <- readGDX('./blabla.gdx', 'VDemTr', field = 'l')
-var_q <- as.quitte(var)
+var_pop <- readGDX('./blabla.gdx', 'iPop')
+var_pop <- as.quitte(var_pop)
+var_pop$model <- "OPEN-PROM"
+var_pop$variable <- "Population"
+var_pop$unit <- "billion"
+var_pop <- select(var_pop, -data)
 
-# write.mif(var_q, 'VDemTr.mif')
+var_gdp <- readGDX('./blabla.gdx', 'iGDP')
+var_gdp <- as.quitte(var_gdp)
+var_gdp$model <- "OPEN-PROM"
+var_gdp$variable <- "GDP"
+var_gdp$unit <- "billion US$2015"
+var_gdp <- select(var_gdp, -data)
 
-iamCheck(var_q, pdf = "./report.pdf",
-         refData = "./Data_validation_mrprom.mif",
-         verbose = FALSE)
+
+#write.mif(var_pop, 'pop.mif')
+
+# iamCheck(var_q, pdf = "./report.pdf",
+#         refData = "./Data_validation_mrprom.mif",
+#         verbose = FALSE)
