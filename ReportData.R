f0a45c381b0b06b218be6c26038a019e97ea2ed4
derevirn <derevirn@gmail.com>
2023-10-18 23:41:01 +0300

    Created data report for GDP and Population

--- ReportData.R
+++ ReportData.R
@@ -3,23 +3,30 @@ library(gdx)
 library(quitte)
 library(iamc)
 
+# Reading data from the GDX file
 var_pop <- readGDX('./blabla.gdx', 'iPop')
 var_pop <- as.quitte(var_pop)
 var_pop$model <- "OPEN-PROM"
 var_pop$variable <- "Population"
-var_pop$unit <- "billion"
+var_pop$unit <- "million"
 var_pop <- select(var_pop, -data)
 
 var_gdp <- readGDX('./blabla.gdx', 'iGDP')
 var_gdp <- as.quitte(var_gdp)
 var_gdp$model <- "OPEN-PROM"
-var_gdp$variable <- "GDP"
-var_gdp$unit <- "billion US$2015"
+var_gdp$variable <- "GDP|PPP"
+var_gdp$unit <- "billion US$2005/yr"
 var_gdp <- select(var_gdp, -data)
 
+# Merging the datasets
+gdx_data <- bind_rows(var_pop, var_gdp)
 
-#write.mif(var_pop, 'pop.mif')
+# Create the data_report folder if it doesn't exist
+if (!file.exists("./data_report"))
+{
+  dir.create("./data_report")
+} 
 
-# iamCheck(var_q, pdf = "./report.pdf",
-#         refData = "./Data_validation_mrprom.mif",
-#         verbose = FALSE)
+report <- iamCheck(gdx_data, pdf = "./data_report/report.pdf",
+          refData = "./GDP_POP.mif",
+          verbose = TRUE)
