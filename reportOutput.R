30eb5beda2602daeeeb6f1b64d30cd5e69a198ef
Fsioutas <fsioutas@hotmail.com>
2023-12-11 15:23:57 +0200

    add price to reportOutput

--- reportOutput.R
+++ reportOutput.R
@@ -25,7 +25,6 @@ VFeCons <- readGDX('./blabla.gdx', "VFeCons", field = 'l')
 VFeCons<-toolAggregate(VFeCons[,,unique(sets$EF)],dim=3,rel=sets,from="EF",to="BAL")
 getItems(VFeCons, 3) <- paste0("Final Energy ", getItems(VFeCons, 3))
 
-
 #add model MENA_EDS data (choosing the correct variable from MENA by use of the MENA-PROM mapping)
 a <- readSource("MENA_EDS", subtype =  map[map[["OPEN.PROM"]] == "VFeCons", "MENA.EDS"])
 getRegions(a) <- sub("MOR", "MAR", getRegions(a)) # fix wrong region names in MENA
@@ -63,3 +62,58 @@ v <- as.quitte(v)
 v <- as.magpie(v)
 # write data in mif file
 write.report(v,file="final.mif",model="ENERDATA",unit="Mtoe",append=TRUE,scenario="BASE")
+
+
+#add model OPEN-PROM data Electricity prices
+VElecPriInduResConsu <- readGDX('./blabla.gdx', "VElecPriInduResConsu", field = 'l')
+#choose Industrial consumer /i/
+sets1 <- readSets("sets.gms", "iSet")
+elec_prices_Industry <- VElecPriInduResConsu[,,sets1[1,1]]
+getNames(elec_prices_Industry) <- "Electricity prices Industrial"
+#choose Residential consumer /r/
+sets2 <- readSets("sets.gms", "rSet")
+elec_prices_Residential <- VElecPriInduResConsu[,,sets2[1,1]]
+getNames(elec_prices_Residential) <- "Electricity prices Residential"
+elec_prices <- mbind(elec_prices_Industry, elec_prices_Residential)
+
+#add model MENA_EDS data (choosing the correct variable from MENA by use of the MENA-PROM mapping)
+z <- readSource("MENA_EDS", subtype =  map[map[["OPEN.PROM"]] == "VElecPriInduResConsu", "MENA.EDS"])
+
+getRegions(z) <- sub("MOR", "MAR", getRegions(z)) # fix wrong region names in MENA
+# choose years and regions that both models have
+years <- intersect(getYears(z,as.integer=TRUE),getYears(VElecPriInduResConsu,as.integer=TRUE))
+regs <- intersect(getRegions(z),getRegions(VElecPriInduResConsu))
+#choose Industrial consumer /i/
+MENA_Industrial <- z[,years,sets1[1,1]]
+getNames(MENA_Industrial) <- "Electricity prices Industrial"
+#choose Residential consumer /r/
+MENA_Residential <- z[,years,sets2[1,1]]
+# complete names
+getNames(MENA_Residential) <- "Electricity prices Residential"
+z <- mbind(MENA_Industrial, MENA_Residential)
+
+# write data in mif file
+write.report(elec_prices[regs,years,],file="elec_prices.mif",model="OPEN-PROM",unit="Euro2005/KWh",scenario="BASE")
+write.report(z[regs,,],file="elec_prices.mif",model="MENA-EDS",unit="Euro2005/KWh",append=TRUE,scenario="BASE")
+
+#filter ENERDATA by lectricity
+k <- readSource("ENERDATA", subtype =  "lectricity", convert = TRUE)
+ENERDATA_Industrial <- k[,,"Constant price per toe in US$ of electricity in industry (taxes incl).$15/toe"]
+ENERDATA_Residential<- k[,,"Constant price per toe in US$ of electricity for households (taxes incl).$15/toe"]
+#fix units from toe to kwh
+ENERDATA_Industrial <- ENERDATA_Industrial / 11630
+ENERDATA_Residential <- ENERDATA_Residential / 11630
+#fix units from $15 to $05
+ENERDATA_Industrial <- ENERDATA_Industrial * 1.2136
+ENERDATA_Residential <- ENERDATA_Residential * 1.2136
+#fix units from $2005 to Euro2005
+ENERDATA_Industrial <- ENERDATA_Industrial / 1.24
+ENERDATA_Residential <- ENERDATA_Residential / 1.24
+
+getItems(ENERDATA_Industrial, 3) <- "Electricity prices Industrial"
+getItems(ENERDATA_Residential, 3) <- "Electricity prices Residential"
+x <- mbind(ENERDATA_Industrial, ENERDATA_Residential)
+x <- x[regs, ,]
+# write data in mif file
+write.report(x,file="elec_prices.mif",model="ENERDATA",unit="Euro2005/KWh",append=TRUE,scenario="BASE")
+
