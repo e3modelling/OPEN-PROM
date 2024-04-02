461960cc30dc70446701ab904896e401b0c6465c
derevirn <derevirn@gmail.com>
2024-04-02 22:39:30 +0300

    Added model metadata + code refactoring.

--- start.R
+++ start.R
@@ -1,12 +1,14 @@
 library(jsonlite)
 
-# Script for OPEN-PROM model execution and other associated tasks.
+### Script for OPEN-PROM model execution and other associated tasks.
 
-# 0 - Define a function that saves metadata about the model run
+withRunFolder = TRUE # Set to FALSE to disable run folder creation and file copying
 
-saveMetadata<- function() {
+### 0 - Define function that saves model metadata into a JSON file.
+
+saveMetadata<- function(DevMode) {
+
   # Gather Git information with system calls
-
   commit_author <- system("git log -1 --format=%an", intern = TRUE)
   commit_hash <- system("git log -1 --format=%H", intern = TRUE)
   commit_comment <- system("git log -1 --format=%B", intern = TRUE)
@@ -21,49 +23,53 @@ saveMetadata<- function() {
     "Date" = commit_date,
     "Branch Name" = branch_name
   )
-  
-  # Wrap the git_info list into another list to create the "Git information" hierarchy
-  data_to_save <- list("Git Information" = git_info)
-  
+ 
+  # Save the appropriate region mapping for each type of run (Development / Research).
+  if(DevMode == 0) {
+    
+    model_info <- list('Region Mapping' = "regionmappingOP5.csv")
+
+    } else if (DevMode == 1) {
+
+      model_info <- list('Region Mapping' = "regionmappingOPDEV2.csv")
+    }
+
   # Convert to JSON and save to file
+  data_to_save <- list("Git Information" = git_info, "Model Information" = model_info)
   json_data <- toJSON(data_to_save, pretty = TRUE)
   write(json_data, file = "metadata.json")
   
-  cat("Git information has been saved to metadata.json\n")
+  cat("Metadata has been saved to metadata.json\n")
+  
 }
 
-# 1 - Creating a separate folder for each model run.
+### 1 - Define a function that creates a separate folder for each model run.
 
-createRunFolder = TRUE # Set to FALSE to disable run folder creation and file copying
+createRunFolder <- function() {
 
-if(createRunFolder) {
+  # generate name of run folder
+  scenario <- "default"
+  folderName <- paste(scenario, format(Sys.time(), "%d-%m-%Y_%H-%M-%S"), sep="_")
 
-# generate metadata file
-saveMetadata()
+  # create run folder under /runs
+  if (!file.exists("runs")) dir.create("runs")
+  runfolder <- paste0("runs/", folderName)
+  dir.create(runfolder)
 
-# generate name of run folder
-scenario <- "default"
-folderName <- paste(scenario, format(Sys.time(), "%d-%m-%Y_%H-%M-%S"), sep="_")
+  # copy necessary files to folder
+  file.copy(grep(".gms$",dir(), value = TRUE), to = runfolder)
+  file.copy(grep(".csv$",dir(), value = TRUE), to = runfolder)
+  file.copy(grep("*.R$",dir(), value = TRUE), to = runfolder)
+  file.copy("conopt.opt", to = runfolder)
+  file.copy("metadata.json", to = runfolder)
+  file.copy("data", to = runfolder, recursive = TRUE)
 
-# create run folder under /runs
-if (!file.exists("runs")) dir.create("runs")
-runfolder <- paste0("runs/", folderName)
-dir.create(runfolder)
-
-# copy necessary files to folder
-file.copy(grep(".gms$",dir(), value = TRUE), to = runfolder)
-file.copy(grep(".csv$",dir(), value = TRUE), to = runfolder)
-file.copy(grep("*.R$",dir(), value = TRUE), to = runfolder)
-file.copy("conopt.opt", to = runfolder)
-file.copy("metadata.json", to = runfolder)
-file.copy("data", to = runfolder, recursive = TRUE)
-
-# switch to the run folder
-setwd(runfolder)
+  # switch to the run folder
+  setwd(runfolder)
 
 }
 
-# 2 - Executing the VS Code tasks
+### 2 - Executing the VS Code tasks
 
 # Parsing the command line argument
 args <- commandArgs(trailingOnly = TRUE)
@@ -81,26 +87,40 @@ for (arg in args) {
 if (!is.null(task) && task == 0) {
 
     # Running task OPEN-PROM DEV
+    saveMetadata(DevMode = 1)
+    if(withRunFolder) { createRunFolder() }
+
     system("gams main.gms --DevMode=1 --GenerateInput=off -logOption 4 -Idir=./data")
 
 } else if (!is.null(task) && task == 1) {
 
     # Running task OPEN-PROM DEV NEW DATA
+    saveMetadata(DevMode = 1)
+    if(withRunFolder) { createRunFolder() }
+
     system("gams main.gms --DevMode=1 --GenerateInput=on -logOption 4 -Idir=./data")
-    if(createRunFolder) {
+
+    if(withRunFolder) {
       file.copy("data", to = '../../', recursive = TRUE) # Copying generated data to parent folder for future runs
       }        
 
 } else if (!is.null(task) && task == 2) {
     
     # Running task OPEN-PROM RESEARCH
+    saveMetadata(DevMode = 0)
+    if(withRunFolder) { createRunFolder() }
+
     system("gams main.gms --DevMode=0 --GenerateInput=off -logOption 4 -Idir=./data")
 
 } else if (!is.null(task) && task == 3) {
     
     # Running task OPEN-PROM RESEARCH NEW DATA
+    saveMetadata(DevMode = 0)
+    if(withRunFolder) { createRunFolder() }
+
     system("gams main.gms --DevMode=0 --GenerateInput=on -logOption 4 -Idir=./data")
-    if(createRunFolder) {
+
+    if(withRunFolder) {
       file.copy("data", to = '../../', recursive = TRUE)
       }    
 }
