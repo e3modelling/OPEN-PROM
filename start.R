ba3bc3881fe54cb2cf3e4b3f74fe95273210dd4c
George Plessias <plessiasgeorge@gmail.com>
2024-08-01 12:54:28 +0300

    VCarVal not(An(YTIME)) removal

--- start.R
+++ start.R
@@ -5,7 +5,7 @@ library(jsonlite)
 withRunFolder = TRUE # Set to FALSE to disable model run folder creation and file copying
 withUpload = TRUE # Set to FALSE to disable model run upload to Google Drive
 withReport = TRUE # Set to FALSE to disable the report output script execution (applicable to research mode only)
-uploadGDX = FALSE # Set to TRUE to include GDX files in the uploaded archive
+uploadGDX = TRUE # Set to TRUE to include GDX files in the uploaded archive
 
 ### Define function that saves model metadata into a JSON file.
 
