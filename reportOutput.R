13a4606ac6e89e35c27c22791c3934817a03a255
Fsioutas <fsioutas@hotmail.com>
2025-10-22 11:14:06 +0300

    create val function

--- reportOutput.R
+++ reportOutput.R
@@ -35,55 +35,20 @@ reportOutput <- function(
 
   reports <- convertGDXtoMIF(runpath,
     mif_name = mif_name,
-    aggregate = aggregate, fullValidation = fullValidation, Validation_data_for_plots = Validation_data_for_plots
+    aggregate = aggregate, fullValidation = fullValidation
   )
   metadata <- getMetadata(path = runpath)
   print("Report generation completed.")
   
-  if (reg_map != "regionmappingOPDEV3.csv") {
-    reports <- reports[[1]]
-  } else {
-    # rename GLO to World
-    reports <- lapply(reports, function(x) {
-      regions <- getRegions(x)
-      if ("GLO" %in% regions) {
-        getRegions(x)[regions == "GLO"] <- "World"
-      }
-      x
-    })
-    # do not take open-prom
-    reports_val <- reports[-1]
-    
-    region_sig <- function(x) paste(sort(getRegions(x)), collapse = "|")
-    sigs <- vapply(reports_val, region_sig, FUN.VALUE = character(1))
-    
-    # Find the most common region set
-    most_common_sig <- names(sort(table(sigs), decreasing = TRUE))[1]
-    
-    # Keep only magpie objects with that region set
-    same_region_list <- reports_val[sigs == most_common_sig]
-    
-    # Combine them
-    if (length(same_region_list) > 1) {
-      combined_all <- do.call(mbind, same_region_list)
-    } else if (length(same_region_list) == 1) {
-      combined_all <- same_region_list[[1]]
-    } else {
-      combined_all <- NULL
-      warning("No magpie objects with identical regions found.")
-    }
-    reports_val <- combined_all
-    reports_val <- as.quitte(reports_val) %>% as.magpie()
-    getItems(reports_val, 3.1) <- paste0(getItems(reports_val, 3.1), "|VAL")
-    
-    reportOPEN_PROM <- reports[[1]]
-    #mbind validation data and OPEN-PROM
-    reports_val <- add_columns(reports_val, addnm = setdiff(getYears(reportOPEN_PROM),getYears(reports_val)), dim = 2, fill = NA)
-    reports_val <- reports_val[,getYears(reportOPEN_PROM),]
-    reports <- mbind(reportOPEN_PROM, reports_val)
-    reports <- list(reports)
-    print(str(reports))
-  }
+  Val_Mif <- ValidationMif(runpath = runpath, reg_map = reg_map, Validation_data_for_plots = Validation_data_for_plots
+  )
+  
+  reportOPEN_PROM <- reports[[1]]
+  #mbind validation data and OPEN-PROM
+  Val_Mif <- add_columns(Val_Mif, addnm = setdiff(getYears(reportOPEN_PROM),getYears(Val_Mif)), dim = 2, fill = NA)
+  Val_Mif <- Val_Mif[,getYears(reportOPEN_PROM),]
+  reports <- mbind(reportOPEN_PROM, Val_Mif)
+  reports <- list(reports)
 
   if (!is.null(plot_name)) {
     save_names <- file.path(runpath, plot_name)
