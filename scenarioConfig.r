3b64e0b35c43ecd98fc1e8bf9e06fdbee93d9d29
Anastasis Giannousakis <anastasis.giannousakis@ricardo.com>
2026-01-14 14:14:36 +0200

    cosmetics

--- scenarioConfig.r
+++ scenarioConfig.r
@@ -3,11 +3,8 @@ library(jsonlite)
 fromcsv <- read.csv("scenarioConfig.csv")
 
 for (i in rownames(fromcsv)) {
-    if (fromcsv[i, "start"] != 1) {
-        next
-    }
-    print(fromcsv[i,])
-    write(toJSON(fromcsv[i,], pretty = TRUE), file = "config2.json")
-    system(paste('Rscript start.R', "task=2"))
+  if (fromcsv[i, "start"] != 1) next
+  print(fromcsv[i, ])
+  write(toJSON(fromcsv[i, ], pretty = TRUE), file = "config2.json")
+  system(paste("Rscript start.R", "task=2"))
 }
-
